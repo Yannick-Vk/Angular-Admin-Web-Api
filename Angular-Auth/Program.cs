0d3225b300706bf2ae7fcb50b3fc843e56323c9f
Yannick Van kerckvoorde <vankerckvoordeyannick@gmail.com>
2025-08-27 08:48:52 +0200

    Added migration

--- Angular-Auth/Program.cs
+++ Angular-Auth/Program.cs
@@ -1,9 +1,14 @@
+using Angular_Auth.Repositories;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+
+// 1. DbContext
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("auth")));
+
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
