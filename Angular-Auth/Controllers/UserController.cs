50a5a4392b65f1982c0fc12abd2e60e6824c04c0
Yannick <vankerckvoordeyannick@gmail.com>
2025-08-27 15:28:55 +0200

    Added authorize

--- Angular-Auth/Controllers/UserController.cs
+++ Angular-Auth/Controllers/UserController.cs
@@ -1,9 +1,11 @@
 ﻿using Angular_Auth.Models;
 using Angular_Auth.Services;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Angular_Auth.Controllers;
 
+[Authorize]
 [ApiController]
 [Route("/api/v1/users")]
 public class UserController(IUserService service) : ControllerBase {
