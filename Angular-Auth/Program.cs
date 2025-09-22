bbd95adcc63b1a78bf4c45de37374ca71ed4a7cc
Yannick <vankerckvoordeyannick@gmail.com>
2025-09-22 14:16:05 +0200

    Revert "Updated to Https"
    This reverts commit 606a0d5898aad6ba336f7ba33274cedfe817fce2.

--- Angular-Auth/Program.cs
+++ Angular-Auth/Program.cs
@@ -129,11 +129,7 @@ using (var scope = app.Services.CreateScope())
 if (app.Environment.IsDevelopment()) {
     app.MapOpenApi();
     app.UseSwagger();
-    app.UseSwaggerUI(c =>
-    {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Angular Blogs API v1");
-        c.SwaggerEndpoint("https://localhost:5001/swagger/v1/swagger.json", "Angular Blogs API v1 - HTTPS");
-    });
+    app.UseSwaggerUI();
 }
 
 // Use CORS
