8da64d40bfd0440db54b176978298cb1982d75ae
Yannick <vankerckvoordeyannick@gmail.com>
2025-10-01 15:32:21 +0200

    Updated to use UserDto instead of string

--- Angular-Auth/Dto/BlogWithAuthor.cs
+++ Angular-Auth/Dto/BlogWithAuthor.cs
@@ -11,7 +11,7 @@ public class BlogWithAuthor {
         Id = blog.Id;
         Title = blog.Title;
         Description = blog.Description;
-        Authors = blog.Authors.Select(user => user.UserName?? "[NULL]").ToList();
+        Authors = blog.Authors.Select(author => new UserDto(author)).ToList();
         CreatedAt = blog.CreatedAt;
         UpdatedAt = blog.UpdatedAt;
     }
@@ -19,7 +19,7 @@ public class BlogWithAuthor {
     public required Guid Id { get; set; }
     public required string Title { get; set; }
     public required string Description { get; set; }
-    public required List<string> Authors { get; set; } = [];
+    public required List<UserDto> Authors { get; set; } = [];
     public required DateTime CreatedAt { get; set; }
     public DateTime? UpdatedAt { get; set; }
 }
