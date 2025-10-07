e53f7194c148621e676605b0e6ae4092f44b2f53
Yannick <vankerckvoordeyannick@gmail.com>
2025-10-07 10:57:53 +0200

    Added helper functions to Get user and check password

--- Angular-Auth/Services/ProfileService.cs
+++ Angular-Auth/Services/ProfileService.cs
@@ -7,18 +7,38 @@ namespace Angular_Auth.Services;
 
 public class ProfileService(UserManager<User> userManager) : IProfileService {
     public async Task UpdateEmail(string userId, string newEmail, string password) {
-        var user = await userManager.FindByIdAsync(userId);
-        if (user == null) {
-            throw new UserNotFoundException($"Cannot find user with id {userId}");
-        }
-
-        var correctPassword = await userManager.CheckPasswordAsync(user, password);
-        if (!correctPassword) throw new WrongCredentialsException("Username and/or password is incorrect");
+        var user = await GetUserOrException(userId);
+        await CheckPasswordOrException(user, password);
 
         var setEmailResult = await userManager.SetEmailAsync(user, newEmail);
         if (!setEmailResult.Succeeded) {
             throw new Exception(
                 $"Failed to update email: {string.Join(", ", setEmailResult.Errors.Select(e => e.Description))}");
         }
+    }
+
+    public async Task UpdatePassword(string userId, string newPassword, string password) {
+        var user = await GetUserOrException(userId);
+        await CheckPasswordOrException(user, password);
+
+        var setEmailResult = await userManager.ChangePasswordAsync(user, password, newPassword);
+        if (!setEmailResult.Succeeded) {
+            throw new Exception(
+                $"Failed to update password: {string.Join(", ", setEmailResult.Errors.Select(e => e.Description))}");
+        }
+    }
+
+    private async Task<User> GetUserOrException(string userId, string? message = null) {
+        var user = await userManager.FindByIdAsync(userId);
+        if (user != null) return user;
+
+        message ??= $"Cannot find user with id {userId}";
+        throw new UserNotFoundException(message);
+    }
+
+    private async Task CheckPasswordOrException(User user, string password, string? message = null) {
+        var correctPassword = await userManager.CheckPasswordAsync(user, password);
+        message ??= "Username and/or password is incorrect";
+        if (!correctPassword) throw new WrongCredentialsException(message);
     }
 }
