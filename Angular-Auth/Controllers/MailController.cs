21e3a3ced70742d8739decb87ef04ff134c3ad0d
Yannick <vankerckvoordeyannick@gmail.com>
2025-10-13 09:02:14 +0200

    Added simple mail send via controller

---
+++ Angular-Auth/Controllers/MailController.cs
@@ -0,0 +1,35 @@
+﻿using MailKit;
+using MailKit.Net.Smtp;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MimeKit;
+
+namespace Angular_Auth.Controllers;
+
+
+[AllowAnonymous]
+[ApiController]
+[Route("api/v1/[controller]")]
+public class MailController : ControllerBase {
+
+    [HttpGet]
+    public async Task<IActionResult> SendMail() {
+        var email = new MimeMessage {
+            Sender = new MailboxAddress("Web api", "web-api@yannick.be"),
+            Subject = "Hello from the Web Api",
+            Body = new TextPart() {
+                Text = "Hello from the Web Api. Welcome to our mail service!"
+            },
+        };
+        email.To.Add(new MailboxAddress("Mr to", "to@example.com"));
+        using var smtp = new SmtpClient();
+        await smtp.ConnectAsync("sandbox.smtp.mailtrap.io", 2525, false);
+        
+        // Note: only needed if the SMTP server requires authentication
+        await smtp.AuthenticateAsync("8dc84259c8b865", "4556e06746fa6b");
+        
+        await smtp.SendAsync(email);
+        await smtp.DisconnectAsync(true);
+        return Ok("Mail has been sent");
+    }
+}
