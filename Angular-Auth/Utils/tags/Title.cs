d6e6188b84318fa85e72e6245c774f2a1a879497
Yannick <vankerckvoordeyannick@gmail.com>
2025-10-13 15:50:10 +0200

    Fixed title ref

--- Angular-Auth/Utils/tags/Title.cs
+++ Angular-Auth/Utils/tags/Title.cs
@@ -3,7 +3,7 @@
 public class TitleTag : HtmlTag {
     private IHtmlTag _content { get; set; }
 
-    public TitleTag(IHtmlTag parent, IHtmlTag content, ref readonly ushort level) : base("h", parent, content) {
+    public TitleTag(IHtmlTag parent, IHtmlTag content, ushort level) : base("h", parent, content) {
         if (level > 6) throw new ArgumentException("Level cannot be bigger than 6.", nameof(level));
         Tag += level.ToString();
         _content = content;
