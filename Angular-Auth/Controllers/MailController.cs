732bf2de50828f7d4046202f734703d4c13f5dec
Yannick <vankerckvoordeyannick@gmail.com>
2025-10-13 09:48:30 +0200

    Added Create Email function and added Send mail dto

--- Angular-Auth/Controllers/MailController.cs
+++ Angular-Auth/Controllers/MailController.cs
@@ -1,4 +1,5 @@
-﻿using MailKit;
+﻿using Angular_Auth.Dto;
+using MailKit;
 using MailKit.Net.Smtp;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -12,33 +13,38 @@ namespace Angular_Auth.Controllers;
 public class MailController : ControllerBase {
     [HttpGet("demo")]
     public async Task<IActionResult> DemoMail(string username, string email) {
-        var htmlBody = "<p>Hey,<br>Just wanted to say hi all the way from the land of C#.<br>-- Code guy</p>";
-        var textBody = """
-                       Hey, Just wanted to say hi all the way from the land of C#.
-                       -- Code guy
-                       """;
-        return await SendEmail(username, email,  "Demo mail from our web api", htmlBody, textBody);
+        return await SendEmail(CreateEmail(new SendMailDto {
+            ToUsername = username,
+            ToEmail = email,
+            Subject = "Demo Mail",
+            Body = new BodyBuilder() {
+                HtmlBody = "<p>Hey,<br>Just wanted to say hi all the way from the land of C#.<br>-- Code guy</p>",
+                TextBody = """
+                           Hey, Just wanted to say hi all the way from the land of C#.
+                           -- Code guy
+                           """,
+            }
+        }));
     }
 
-    private async Task<IActionResult> SendEmail(string username, string emailAddress, string subject, string htmlBody, string textBody) {
-        var email = new MimeMessage();
-        email.From.Add(new MailboxAddress("Web Api", "js-blogger@yannick.be"));
-        email.To.Add(new MailboxAddress(username, emailAddress));
-        email.Subject = subject; 
+    private async Task<IActionResult> SendEmail(MimeMessage email) {
         using var smtp = new SmtpClient();
         await smtp.ConnectAsync("sandbox.smtp.mailtrap.io", 2525, false);
 
         // Note: only needed if the SMTP server requires authentication
         await smtp.AuthenticateAsync("8dc84259c8b865", "4556e06746fa6b");
-        var bodyBuilder = new BodyBuilder {
-            HtmlBody = htmlBody,
-            TextBody = textBody, 
-        };
-
-        email.Body = bodyBuilder.ToMessageBody();
 
         await smtp.SendAsync(email);
         await smtp.DisconnectAsync(true);
-        return Ok($"Mail '{email.Subject}' has been sent to {String.Join(",", email.To)}");
+        return Ok($"Mail '{email.Subject}' has been sent to {string.Join(",", email.To)}");
+    }
+
+    private MimeMessage CreateEmail(SendMailDto mail) {
+        var email = new MimeMessage();
+        email.From.Add(new MailboxAddress("Web Api", "js-blogger@yannick.be"));
+        email.To.Add(new MailboxAddress(mail.ToUsername, mail.ToEmail));
+        email.Subject = mail.Subject;
+        email.Body = mail.Body.ToMessageBody();
+        return email;
     }
 }
---
+++ Angular-Auth/Dto/SendMailDto.cs
@@ -0,0 +1,12 @@
+﻿using MimeKit;
+
+namespace Angular_Auth.Dto;
+
+public class SendMailDto {
+    public required string ToUsername { get; init; }
+    public required string ToEmail { get; init; }
+    public required string Subject { get; init; }
+    public required BodyBuilder Body { get; init; } 
+    
+    
+}
