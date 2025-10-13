a332ebe531efaa39c14a1dfb9630c81023b8ab50
Yannick <vankerckvoordeyannick@gmail.com>
2025-10-13 09:31:13 +0200

    Added send mail function

--- Angular-Auth/Controllers/MailController.cs
+++ Angular-Auth/Controllers/MailController.cs
@@ -11,23 +11,28 @@ namespace Angular_Auth.Controllers;
 [Route("api/v1/[controller]")]
 public class MailController : ControllerBase {
     [HttpGet]
-    public async Task<IActionResult> SendMail() {
+    public async Task<IActionResult> DemoMail(string username, string email) {
+        var htmlBody = "<p>Hey,<br>Just wanted to say hi all the way from the land of C#.<br>-- Code guy</p>";
+        var textBody = """
+                       Hey, Just wanted to say hi all the way from the land of C#.
+                       -- Code guy
+                       """;
+        return await SendEmail(username, email,  "Demo mail from our web api", htmlBody, textBody);
+    }
+
+    private async Task<IActionResult> SendEmail(string username, string emailAddress, string subject, string htmlBody, string textBody) {
         var email = new MimeMessage();
         email.From.Add(new MailboxAddress("Web Api", "js-blogger@yannick.be"));
-        email.To.Add(new MailboxAddress("Mr to", "to@example.com"));
-
-        email.Subject = "Hello from the web api";
+        email.To.Add(new MailboxAddress(username, emailAddress));
+        email.Subject = subject; 
         using var smtp = new SmtpClient();
         await smtp.ConnectAsync("sandbox.smtp.mailtrap.io", 2525, false);
 
         // Note: only needed if the SMTP server requires authentication
         await smtp.AuthenticateAsync("8dc84259c8b865", "4556e06746fa6b");
         var bodyBuilder = new BodyBuilder {
-            HtmlBody = "<p>Hey,<br>Just wanted to say hi all the way from the land of C#.<br>-- Code guy</p>",
-            TextBody = """
-                       Hey, Just wanted to say hi all the way from the land of C#.
-                       -- Code guy
-                       """,
+            HtmlBody = htmlBody,
+            TextBody = textBody, 
         };
 
         email.Body = bodyBuilder.ToMessageBody();
