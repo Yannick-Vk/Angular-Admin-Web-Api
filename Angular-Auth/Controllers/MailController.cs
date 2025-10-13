bf5b65ba531bd0e9a71807a65af498148bbb8859
Yannick <vankerckvoordeyannick@gmail.com>
2025-10-13 09:14:23 +0200

    Added plain text falltrough with a bodyBuilder

--- Angular-Auth/Controllers/MailController.cs
+++ Angular-Auth/Controllers/MailController.cs
@@ -6,28 +6,32 @@ using MimeKit;
 
 namespace Angular_Auth.Controllers;
 
-
 [AllowAnonymous]
 [ApiController]
 [Route("api/v1/[controller]")]
 public class MailController : ControllerBase {
-
     [HttpGet]
     public async Task<IActionResult> SendMail() {
-        var email = new MimeMessage(); 
+        var email = new MimeMessage();
         email.From.Add(new MailboxAddress("Web Api", "js-blogger@yannick.be"));
         email.To.Add(new MailboxAddress("Mr to", "to@example.com"));
-        
+
         email.Subject = "Hello from the web api";
-        email.Body = new TextPart(MimeKit.Text.TextFormat.Html) { 
-            Text = $"<html><h1>Welcome from the web api!</h1><b>Hello all the way from the land of C#</b><i>{DateTime.Now}</i></html>"
-        };
         using var smtp = new SmtpClient();
         await smtp.ConnectAsync("sandbox.smtp.mailtrap.io", 2525, false);
-        
+
         // Note: only needed if the SMTP server requires authentication
         await smtp.AuthenticateAsync("8dc84259c8b865", "4556e06746fa6b");
-        
+        var bodyBuilder = new BodyBuilder {
+            HtmlBody = "<p>Hey,<br>Just wanted to say hi all the way from the land of C#.<br>-- Code guy</p>",
+            TextBody = """
+                       Hey, Just wanted to say hi all the way from the land of C#.
+                       -- Code guy
+                       """,
+        };
+
+        email.Body = bodyBuilder.ToMessageBody();
+
         await smtp.SendAsync(email);
         await smtp.DisconnectAsync(true);
         return Ok("Mail has been sent");
