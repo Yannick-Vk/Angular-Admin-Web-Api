fe45f20df7284fdd87f799d73455f8475775aca6
Yannick <vankerckvoordeyannick@gmail.com>
2025-10-13 10:41:36 +0200

    Added overload for create to include from data

--- Angular-Auth/Services/Interfaces/IMailService.cs
+++ Angular-Auth/Services/Interfaces/IMailService.cs
@@ -4,12 +4,16 @@ using MimeKit;
 namespace Angular_Auth.Services.Interfaces;
 
 public interface IMailService {
+    private const string FromName = "JS Blogger";
+    private const string FromAdress = "js-blogger@yannick.be";
     public Task SendEmail(MimeMessage email);
     public Task SendEmail(SendMailDto dto) => SendEmail(CreateEmail(dto));
 
-    public static MimeMessage CreateEmail(SendMailDto dto) {
+    public static MimeMessage CreateEmail(SendMailDto dto) => CreateEmail(dto, (FromName, FromAdress)); 
+    
+    public static MimeMessage CreateEmail(SendMailDto dto, (string name, string address) from) {
         var email = new MimeMessage();
-        email.From.Add(new MailboxAddress("Web Api", "js-blogger@yannick.be"));
+        email.From.Add(new MailboxAddress(from.name, from.address));
         email.To.Add(new MailboxAddress(dto.ToUsername, dto.ToEmail));
         email.Subject = dto.Subject;
         email.Body = dto.Body.ToMessageBody();
