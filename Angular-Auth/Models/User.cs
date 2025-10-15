93e31488cbdc6212bbcacbc41c7c5e9cbf9d874e
Yannick <vankerckvoordeyannick@gmail.com>
2025-10-15 11:30:39 +0200

    Added email verification token field and migration

--- Angular-Auth/Models/User.cs
+++ Angular-Auth/Models/User.cs
@@ -7,4 +7,6 @@ public class User : IdentityUser {
     [MaxLength(50)]
     public string? RefreshToken { get; set; }
     public DateTime RefreshTokenExpiryTime { get; set; }
+    [MaxLength(100)]
+    public string EmailVerificationToken { get; set; }
 }
