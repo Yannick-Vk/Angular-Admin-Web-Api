ab9f245b4cd52fbdca0cb6bb6999302348f0ea65
Yannick <vankerckvoordeyannick@gmail.com>
2025-10-15 16:01:30 +0200

    Added overload for string values

--- Angular-Auth/Utils/HtmlBuilder.cs
+++ Angular-Auth/Utils/HtmlBuilder.cs
@@ -9,27 +9,26 @@ public class HtmlBuilder {
         return Tree.ToString();
     }
 
-    public void AddTitle(Text text, ushort level) {
-        AddTitle(text.ToTag(), level);
-    }
-
     public void AddTitle(IHtmlTag tag, ushort level) {
         Tree.Add(new TitleTag(tag, level));
     }
+
+    public void AddTitle(Text text, ushort level) => AddTitle(text.ToTag(), level);
+
+    public void AddTitle(string title, ushort level) => AddTitle(new Text(title), level);
 
     public void AddParagraph(IHtmlTag tag) {
         Tree.Add(new Paragraph(tag));
     }
 
-    public void AddParagraph(Text text) {
-        AddParagraph(text.ToTag());
-    }
+    public void AddParagraph(Text text) => AddParagraph(text.ToTag());
 
-    public void AddLink(Text text, string link) {
-        AddLink(text.ToTag(), link);
-    }
+    public void AddParagraph(string text) => AddParagraph(new Text(text));
 
     public void AddLink(IHtmlTag tag, string link) {
         Tree.Add(new Link(tag, link));
     }
+
+    public void AddLink(Text text, string link) => AddLink(text.ToTag(), link);
+    public void AddLink(string text, string link) => AddLink(new Text(text), link);
 }
