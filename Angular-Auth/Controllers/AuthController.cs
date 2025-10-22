e98f7bc2418ae6db3f8ef52f57bc58f000b0f68e
Yannick <vankerckvoordeyannick@gmail.com>
2025-10-22 08:50:49 +0200

    Set user claims

--- Angular-Auth/Controllers/AuthController.cs
+++ Angular-Auth/Controllers/AuthController.cs
@@ -1,3 +1,4 @@
+using System.Security.Claims;
 using Angular_Auth.Dto;
 using Angular_Auth.Dto.Auth;
 using Angular_Auth.Exceptions;
@@ -6,6 +7,7 @@ using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.OAuth;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using OpenIddict.Abstractions;
 using OpenIddict.Client.WebIntegration;
 using IAuthenticationService = Angular_Auth.Services.Interfaces.IAuthenticationService;
 
@@ -99,12 +101,49 @@ public class AuthController(
 
     [AllowAnonymous]
     [HttpGet("callback/login/github")]
-    public async Task<IResult> GithubCallbackLogin() {
+    public async Task<IActionResult> GithubCallbackLogin() {
         logger.LogInformation("Github callback login");
         var result = await HttpContext.AuthenticateAsync(OpenIddictClientWebIntegrationConstants.Providers.GitHub);
 
-        return Results.Text(string.Format("{0} has {1} public repositories.",
-            result.Principal!.FindFirst("name")!.Value,
-            result.Principal!.FindFirst("public_repos")!.Value));
+        // Build an identity based on the external claims and that will be used to create the authentication cookie.
+        var identity = new ClaimsIdentity(authenticationType: "ExternalLogin");
+
+        // By default, OpenIddict will automatically try to map the email/name and name identifier claims from
+        // their standard OpenID Connect or provider-specific equivalent, if available. If needed, additional
+        // claims can be resolved from the external identity and copied to the final authentication cookie.
+        identity.SetClaim(ClaimTypes.Email, result.Principal!.GetClaim(ClaimTypes.Email))
+            .SetClaim(ClaimTypes.Name, result.Principal!.GetClaim(ClaimTypes.Name))
+            .SetClaim(ClaimTypes.NameIdentifier, result.Principal!.GetClaim(ClaimTypes.NameIdentifier));
+
+        // Preserve the registration details to be able to resolve them later.
+        identity.SetClaim(OpenIddictConstants.Claims.Private.RegistrationId,
+                result.Principal!.GetClaim(OpenIddictConstants.Claims.Private.RegistrationId))
+            .SetClaim(OpenIddictConstants.Claims.Private.ProviderName,
+                result.Principal!.GetClaim(OpenIddictConstants.Claims.Private.ProviderName));
+
+        // Build the authentication properties based on the properties that were added when the challenge was triggered.
+        var properties = new AuthenticationProperties(result.Properties.Items) {
+            RedirectUri = result.Properties.RedirectUri ?? "/whoami"
+        };
+        
+        logger.LogInformation("Redirecting to {redirectUri}", properties.RedirectUri);
+
+        // Ask the default sign-in handler to return a new cookie and redirect the
+        // user agent to the return URL stored in the authentication properties.
+        //
+        // For scenarios where the default sign-in handler configured in the ASP.NET Core
+        // authentication options shouldn't be used, a specific scheme can be specified here.
+        return SignIn(new ClaimsPrincipal(identity), properties);
+    }
+
+    [AllowAnonymous]
+    [HttpGet("whoami")]
+    public async Task<IActionResult> WhoAmI() {
+        var result = await HttpContext.AuthenticateAsync();
+        if (result is not { Succeeded: true }) {
+            return Ok("You're not logged in.");
+        }
+
+        return Ok($"You are {result.Principal.FindFirst(ClaimTypes.Name)!.Value}.");
     }
 }
