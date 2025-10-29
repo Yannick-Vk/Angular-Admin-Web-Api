1622796668bc35f684398ce4945338f8a8fbfa67
Yannick <vankerckvoordeyannick@gmail.com>
2025-10-29 14:51:10 +0100

    Created EmailBuilder project to create sample email's

---
+++ MailBuilder/Program.cs
@@ -0,0 +1,20 @@
+﻿using Angular_Auth.Utils;
+
+internal class Program {
+    public static async Task Main(string[] args) {
+        var path = "../../../Mails";
+        var name = "demo";
+
+        Console.WriteLine($"Creating Mail {name} in : '{path}'");
+
+        var mail = new MailBodyBuilder()
+            .AddTitle("Demo")
+            .AddDiv(builder => builder
+                .AddParagraph("This is a demo build")
+                .AddLink("Link to github", "https://github.com")
+            );
+        await mail.ToFiles(path, name);
+
+        Console.WriteLine("Created Mail!");
+    }
+}
